8b92b306ff3c86f23e7615b89b390744ac6b51bc
Daniel Ruiz de Alegría <daniruiz@localhost.localdomain>
2019-01-06 23:31:13 +0100

    Add gresource compiled version

--- src/build.sh
+++ src/build.sh
@@ -1,26 +1,43 @@
 #!/bin/sh
 
 TMP="/tmp/flat-remix-gnome"
+
+rm -rf "$TMP"
 mkdir -p "$TMP"
 
+# Build CSS files
 for scss in sass/*.scss
 do
-  scss --sourcemap=none -C -q $scss "$TMP"/$(basename ${scss%%.scss}).css
+  scss --sourcemap=none -C -q "$scss" "$TMP"/"$(basename ${scss%%.scss})".css
 done
 
-
-cp -f "$TMP"/gnome-shell.css ../Flat-Remix/gnome-shell/gnome-shell.css
-cp -f "$TMP"/gnome-shell-dark.css ../Flat-Remix-Dark/gnome-shell/gnome-shell.css
-cp -f "$TMP"/gnome-shell-darkest.css ../Flat-Remix-Darkest/gnome-shell/gnome-shell.css
-cp -f "$TMP"/gnome-shell-miami.css ../Flat-Remix-Miami/gnome-shell/gnome-shell.css
-cp -f "$TMP"/gnome-shell-miami-dark.css ../Flat-Remix-Miami-Dark/gnome-shell/gnome-shell.css
-
-for i in assets/*
+for css in "$TMP"/Flat-Remix*.css
 do
-  cp -rf "$i"/* "../${i#assets/}/gnome-shell/assets/"
+ cp -f "$css" ../"$(basename "${css%.css}")"/gnome-shell/gnome-shell.css
 done
 
+# Copy assets
+for assets in assets/*
+do
+  target=../"${assets#assets/}"/gnome-shell/assets
+  rm -f "$target"/*
+  cp -rf "$assets"/* "$target"
+done
+
+# Generate gresource files
+cp -r assets/* "$TMP"
+sed "s/assets/resource:\/\/\/org\/gnome\/shell\/theme/g" -i "$TMP"/*.css
+for theme in "$TMP"/Flat-Remix*/
+do
+  cp gnome-shell-theme.gresource.xml "$theme"
+  cp "$TMP"/"$(basename ${theme%/})".css "$theme"gnome-shell.css
+  cp "$TMP"/gnome-shell-high-contrast.css "$theme"
+  cp pad-osd.css "$theme"
+  (cd "$theme" && glib-compile-resources gnome-shell-theme.gresource.xml)
+  cp "$theme"/gnome-shell-theme.gresource ../$(basename "$theme")
+done
+
+# Install new themes
 sudo cp -rf ../Flat-Remix* /usr/share/themes
 
-gsettings set org.gnome.shell.extensions.user-theme name "Flat-Remix"
 gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell --method org.gnome.Shell.Eval 'Main.loadTheme();'
