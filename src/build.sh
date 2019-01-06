3536f309b60e91eda96c10be43241445206bb3b5
Daniel Ruiz de Alegría <daniruiz@localhost.localdomain>
2019-01-06 21:14:43 +0100

    Move scss related files to src/sass directory

--- src/build.sh
+++ src/build.sh
@@ -3,9 +3,9 @@
 TMP="/tmp/flat-remix-gnome"
 mkdir -p "$TMP"
 
-for scss in *.scss
+for scss in sass/*.scss
 do
-  scss --sourcemap=none -q $scss "$TMP"/${scss%%.scss}.css
+  scss --sourcemap=none -C -q $scss "$TMP"/$(basename ${scss%%.scss}).css
 done
 
 
@@ -15,7 +15,12 @@ cp -f "$TMP"/gnome-shell-darkest.css ../Flat-Remix-Darkest/gnome-shell/gnome-she
 cp -f "$TMP"/gnome-shell-miami.css ../Flat-Remix-Miami/gnome-shell/gnome-shell.css
 cp -f "$TMP"/gnome-shell-miami-dark.css ../Flat-Remix-Miami-Dark/gnome-shell/gnome-shell.css
 
-sudo cp -Rf ../Flat-Remix* /usr/share/themes
+for i in assets/*
+do
+  cp -rf "$i"/* "../${i#assets/}/gnome-shell/assets/"
+done
+
+sudo cp -rf ../Flat-Remix* /usr/share/themes
 
 gsettings set org.gnome.shell.extensions.user-theme name "Flat-Remix"
 gdbus call --session --dest org.gnome.Shell --object-path /org/gnome/Shell --method org.gnome.Shell.Eval 'Main.loadTheme();'
