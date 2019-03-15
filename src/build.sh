5b00b03dfce3bbd747ffd108303e25b181547484
Daniel Ruiz de Alegría <daniel@drasite.com>
2019-03-15 19:16:08 +0100

    Update build.sh script to modify login-background

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,33 @@
 #!/bin/sh
 
+USAGE="$(basename "$0") [--login-background FILE]
+    Generate Flat-Remix gnome-shell theme variants
+
+    Options:
+      --login-background FILE \t use a custom login background image
+      -h, --help              \t show this help text"
+
+
 TMP="/tmp/flat-remix-gnome"
+LOGIN_BACKGROUND=''
+
+while [[ $# -gt 0 ]]
+do
+	key="$1"
+
+	case $key in
+		--login-background)
+			LOGIN_BACKGROUND="$2"
+			shift # past argument
+			shift # past value
+			;;
+		-h|--help)
+			echo -e "$USAGE"
+			exit 0
+			;;
+	esac
+done
+
 
 rm -rf "$TMP"
 mkdir -p "$TMP"
@@ -8,7 +35,7 @@ mkdir -p "$TMP"
 # Build CSS files
 for scss in sass/*.scss
 do
-  scss --sourcemap=none -C -q "$scss" "$TMP"/"$(basename ${scss%%.scss})".css
+	scss --sourcemap=none -C -q "$scss" "$TMP"/"$(basename ${scss%%.scss})".css
 done
 
 for css in "$TMP"/Flat-Remix*.css
@@ -19,9 +46,10 @@ done
 # Copy assets
 for assets in assets/*
 do
-  target=../"${assets#assets/}"/gnome-shell/assets
-  rm -f "$target"/*
-  cp -rf "$assets"/* "$target"
+	target=../"${assets#assets/}"/gnome-shell/assets
+	rm -f "$target"/*
+	cp -rf "$assets"/* "$target"
+	[[ $LOGIN_BACKGROUND != '' ]] && cp -f "$LOGIN_BACKGROUND" "$target"/login-background
 done
 
 # Generate gresource files
@@ -29,10 +57,11 @@ cp -r assets/* "$TMP"
 sed "s/assets/resource:\/\/\/org\/gnome\/shell\/theme/g" -i "$TMP"/*.css
 for theme in "$TMP"/Flat-Remix*/
 do
-  cp gnome-shell-theme.gresource.xml "$theme"
-  cp "$TMP"/"$(basename ${theme%/})".css "$theme"gnome-shell.css
-  cp "$TMP"/gnome-shell-high-contrast.css "$theme"
-  cp pad-osd.css "$theme"
-  (cd "$theme" && glib-compile-resources gnome-shell-theme.gresource.xml)
-  cp "$theme"/gnome-shell-theme.gresource ../$(basename "$theme")
+	[[ $LOGIN_BACKGROUND != '' ]] && cp -f "$LOGIN_BACKGROUND" "$theme"/login-background
+	cp gnome-shell-theme.gresource.xml "$theme"
+	cp "$TMP"/"$(basename ${theme%/})".css "$theme"gnome-shell.css
+	cp "$TMP"/gnome-shell-high-contrast.css "$theme"
+	cp pad-osd.css "$theme"
+	(cd "$theme" && glib-compile-resources gnome-shell-theme.gresource.xml)
+	cp "$theme"/gnome-shell-theme.gresource ../$(basename "$theme")
 done
