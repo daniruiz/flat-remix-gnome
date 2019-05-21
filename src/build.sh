c197f1502a7364dd93417c5fae8e1da8d726e583
Daniel Ruiz de Alegría <daniel@drasite.com>
2019-05-21 09:27:53 +0200

    Convert build.sh script to posix sh

--- src/build.sh
+++ src/build.sh
@@ -1,21 +1,23 @@
-#!/bin/bash
+#!/bin/sh
 
-USAGE="$(basename "$0") [--login-background FILE]
+USAGE="
+$(basename "$0") [--login-background FILE]
     Generate Flat-Remix gnome-shell theme variants
 
     Options:
-      --login-background FILE \t use a custom login background image
-      --sync-login-background \t use gnome settings lock background as login background image
-      --blur N                \t apply gaussian blur to login background image (default 2.5)
-      -r, --rebuild-theme     \t regenerate theme CSS files
-      -h, --help              \t show this help text"
+      --login-background FILE    use a custom login background image
+      --sync-login-background    use gnome settings lock background as login background image
+      --blur N                   apply gaussian blur to login background image (default 2.5)
+      -r, --rebuild-theme        regenerate theme CSS files
+      -h, --help                 show this help text
+"
 
 
 TMP="/tmp/flat-remix-gnome"
 LOGIN_BACKGROUND=''
 BLUR=6
 
-while [[ $# -gt 0 ]]
+while [ $# -gt 0 ]
 do
 	key="$1"
 
@@ -39,7 +41,7 @@ do
 			shift # past value
 			;;
 		-h|--help)
-			echo -e "$USAGE"
+			echo "$USAGE"
 			exit 0
 			;;
 	esac
@@ -50,11 +52,11 @@ rm -rf "$TMP"
 mkdir -p "$TMP"
 
 # Build CSS files
-if (( $REBUILD_CSS ))
+if [ -n "$REBUILD_CSS" ]
 then
 	for scss in sass/*.scss
 	do
-		scss --sourcemap=none -C -q "$scss" css/"$(basename ${scss%%.scss})".css
+		scss --sourcemap=none -C -q "$scss" css/"$(basename "${scss%%.scss}")".css
 	done
 fi
 
@@ -64,12 +66,12 @@ do
 done
 
 # Copy assets
-(( $SYNC_LOGIN_BACKGROUND )) && LOGIN_BACKGROUND=$(dconf read /org/gnome/desktop/screensaver/picture-uri | sed -e "s/file:\/\///" -e "s/'//g")
-if [[ $LOGIN_BACKGROUND != '' ]]
+[ -n "$SYNC_LOGIN_BACKGROUND" ] && LOGIN_BACKGROUND=$(dconf read /org/gnome/desktop/screensaver/picture-uri | sed -e "s/file:\/\///" -e "s/'//g")
+if [ -n "$LOGIN_BACKGROUND" ]
 then
-	if [[ $BLUR < 1 || $BLUR == 1 ]]
-		then convert -gaussian-blur 0x${BLUR} $LOGIN_BACKGROUND "$TMP"/login-background;
-		else convert -scale 10% -gaussian-blur 0x${BLUR} -resize 1000% $LOGIN_BACKGROUND "$TMP"/login-background;
+	if [ "$BLUR" -lt 1 ] || [ "$BLUR" -eq 1 ]
+		then convert -gaussian-blur 0x"${BLUR}" "$LOGIN_BACKGROUND" "$TMP"/login-background;
+		else convert -scale 10% -gaussian-blur 0x"${BLUR}" -resize 1000% "$LOGIN_BACKGROUND" "$TMP"/login-background;
 	fi
 fi
 for assets in assets/*
@@ -77,20 +79,20 @@ do
 	target=../"${assets#assets/}"/gnome-shell/assets
 	rm -f "$target"/*
 	cp -rf "$assets"/* "$target"
-	[[ $LOGIN_BACKGROUND != '' ]] && cp -f "$TMP"/login-background "$target"/login-background
+	[ -n "$LOGIN_BACKGROUND" ] && cp -f "$TMP"/login-background "$target"/login-background
 done
 
 # Generate gresource files
 cp -r assets/* "$TMP"
 cp css/* "$TMP"
 sed "s/assets/resource:\/\/\/org\/gnome\/shell\/theme/g" -i "$TMP"/*.css
-for theme in "$TMP"/Flat-Remix*/
+find "$TMP" -name "Flat-Remix*" -type d | while IFS= read -r theme
 do
-	[[ $LOGIN_BACKGROUND != '' ]] && cp -f "$TMP"/login-background "$theme"/login-background
+	[ -n "$LOGIN_BACKGROUND" ] && cp -f "$TMP"/login-background "$theme"/login-background
 	cp gnome-shell-theme.gresource.xml "$theme"
-	cp "$TMP"/"$(basename ${theme%/})".css "$theme"gnome-shell.css
+	cp "$TMP"/"$(basename "$theme")".css "$theme"/gnome-shell.css
 	cp "$TMP"/gnome-shell-high-contrast.css "$theme"
 	cp pad-osd.css "$theme"
 	(cd "$theme" && glib-compile-resources gnome-shell-theme.gresource.xml)
-	cp "$theme"/gnome-shell-theme.gresource ../$(basename "$theme")
+	cp "$theme"/gnome-shell-theme.gresource ../"$(basename "$theme")"
 done
